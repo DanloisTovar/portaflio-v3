4bbb2e53ec1f33d5735bf2b5bee0e17f3bb84fa6
Danlois Tovar <danlois.tovar@gmail.com>
2025-11-05 19:50:36 +0100

    chore(package): 🎉 actualizar package.json con scripts E2E y deps se borro test innecesario

--- testing/pruebaTesting.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe } from 'node:test';
-import { expect, test } from 'vitest';
-
-describe('Prueba de test unitario', () => {
-	test('Caso de prueba de muestra', () => {
-		expect(2 + 1).toBe(3);
-	});
-});
